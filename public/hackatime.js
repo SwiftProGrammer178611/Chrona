5cbe1cca242c4bf8c235f6518e05119b6694afb9
SwiftProGrammer178611 <165575838+SwiftProGrammer178611@users.noreply.github.com>
2026-07-14 21:12:01 -0400

    hackatime.js changes, realized there was a typo

--- public/hackatime.js
+++ public/hackatime.js
@@ -2,7 +2,7 @@ async function getHackatimeStats() {
     const storedToken = localStorage.getItem("hackatimetok");
     const res = await fetch("/api/stats", {
         cache: "no-store",
-        headers: storedToken ? {"Authroization": `Bearer ${storedToken}`} : {}
+        headers: storedToken ? {"Authorization": `Bearer ${storedToken}`} : {}
     });
     if(!res.ok) throw new Error("Couldn't load stats");
     return await res.json();
