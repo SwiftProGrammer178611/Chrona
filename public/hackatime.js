0fc3c1c1f74c0266759ffe7cbb8f27f022d01ea4
SwiftProGrammer178611 <165575838+SwiftProGrammer178611@users.noreply.github.com>
2026-07-14 21:08:41 -0400

    hackatime.js changes to async func

--- public/hackatime.js
+++ public/hackatime.js
@@ -1,5 +1,9 @@
-async function getHackatimeStats(){ 
-    const res = await fetch("/api/stats", {cache:"no-store"});
+async function getHackatimeStats() {
+    const storedToken = localStorage.getItem("hackatimetok");
+    const res = await fetch("/api/stats", {
+        cache: "no-store",
+        headers: storedToken ? {"Authroization": `Bearer ${storedToken}`} : {}
+    });
     if(!res.ok) throw new Error("Couldn't load stats");
     return await res.json();
 }
